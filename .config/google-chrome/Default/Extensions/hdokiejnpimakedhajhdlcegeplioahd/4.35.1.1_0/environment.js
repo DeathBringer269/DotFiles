1f04e1f0fb4383da7d472a5157a333503b4b0d3e
Alan Dsilva <deathbringer2604@gmail.com>
2019-11-30 15:00:14 +0000

    removed unchanged files and folders

--- .config/google-chrome/Default/Extensions/hdokiejnpimakedhajhdlcegeplioahd/4.35.1.1_0/environment.js
+++
@@ -1,2 +0,0 @@
-var Environment={version:"4.35.1.1",environment:"prod",lastpass_url:"https://lastpass.com/"};
-//# sourceMappingURL=sourcemaps/environment.js.map
--- .config/rofi/run_rofi.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-theme=${1:-$HOME/.config/rofi/config.rasi}
-selection=$(echo -e "${options}" | rofi -dmenu -config $theme)
